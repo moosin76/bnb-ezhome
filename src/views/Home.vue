c85c01556215eee3349b0444eaaba172be7ac3fc
Kiyong Nam <moosin76@gmail.com>
2021-07-11 15:49:50 +0900

    프로그레스바

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,12 @@
 			<v-btn @click="toastTest4">토스트 Warning</v-btn>
 			<v-btn @click="toastTest5">전역 에러</v-btn>
 		</div>
+		<h1>프로그레스바 테스트</h1>
+		<div>
+			<v-btn @click="progressTest1">Start</v-btn>
+			<v-btn @click="progressTest2">Finish</v-btn>
+			<v-btn @click="progressTest3">Fail</v-btn>
+		</div>
 	</div>
 </template>
 
@@ -29,6 +35,15 @@ export default {
 		},
 		toastTest5() {
 			throw new Error('에러가 발생');
+		},
+		progressTest1() {
+			this.$Progress.start();
+		},
+		progressTest2() {
+			this.$Progress.finish();
+		},
+		progressTest3() {
+			this.$Progress.fail();
 		},
 	}
 };
