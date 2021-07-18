613a9da33874d3b3fe5e77d52df21fbf374826cc
Kiyong Nam <moosin76@gmail.com>
2021-07-18 17:03:14 +0900

    title footer

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,10 @@ Vue.use(Vuex)
 export function createStore() {
 	const store = new Vuex.Store({
 		state: {
+			config: {
+				title : "ezCode Home",
+				footer: "ezCode all right reserved.",
+			}
 		},
 		mutations: {
 		},
