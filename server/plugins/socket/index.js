4c556485d838462ecc07b40f55779b182ba48e21
Kiyong Nam <moosin76@gmail.com>
2021-12-05 16:17:25 +0900

    소켓 io redis 아답터

--- server/plugins/socket/index.js
+++ server/plugins/socket/index.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const { Server } = require("socket.io");
+const redisAdapter = require('socket.io-redis');
 const configHandler = require('./configHandler');
+
+const { REDIS_HOST, REDIS_PORT } = process.env;
 
 module.exports = function (webServer) {
 	const io = new Server(webServer);
+	io.adapter(redisAdapter({ host: REDIS_HOST, port: REDIS_PORT }));
 
 	io.on("connection", (socket) => {
 		configHandler(io, socket);
@@ -13,22 +18,22 @@ module.exports = function (webServer) {
 			console.log('disconnect');
 		});
 
-		socket.on('room:join', (roomName)=>{
+		socket.on('room:join', (roomName) => {
 			console.log("room:join", roomName);
 			socket.join(roomName);
 		});
-		
-		socket.on('room:leave', (roomName)=>{
+
+		socket.on('room:leave', (roomName) => {
 			console.log("room:leave", roomName);
 			socket.leave(roomName);
 		});
 
-		socket.on('room:send', (data)=>{
+		socket.on('room:send', (data) => {
 			console.log(data);
 			const msg = data.msg + " 서버 응답";
 			// io.emit('room:msg', {msg}); // 모든 사용자
 			// socket.broadcast.emit('room:msg', {msg}); // 나를 제외한 모든 사용자
-			io.to('roomtest').emit('room:msg', {msg}); // 룸에 있는 모든 사용자
+			io.to('roomtest').emit('room:msg', { msg }); // 룸에 있는 모든 사용자
 			// socket.to('roomtest').emit('room:msg', {msg});	// 룸에있는 나를 제외한 모든 사용자
 		})
 	})
