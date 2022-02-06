e5670775c92c6853ea911bd53d063304efabe7af
Kiyong Nam <moosin76@gmail.com>
2022-02-06 17:05:40 +0900

    063 - 회원정보 출력, 테마이이콘

--- src/components/layout/SiteUser.vue
+++ src/components/layout/SiteUser.vue
@@ -8,17 +8,22 @@
         </v-btn>
       </template>
       <v-card>
-        <v-card-text>
-          <v-switch
-            label="Dark Theme"
-            :input-value="darkMode"
-            @change="setDarkMode($event)"
-          ></v-switch>
-        </v-card-text>
+				<v-sheet width="200">
         <member-menu v-if="member" @open="openDialog" />
         <no-member-menu v-else />
+				</v-sheet>
       </v-card>
     </v-menu>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-on="on" v-bind="attrs" icon @click="setDarkMode(!darkMode)">
+          <v-icon v-if="darkMode">mdi-lightbulb-on-outline</v-icon>
+          <v-icon v-else>mdi-lightbulb-outline</v-icon>
+        </v-btn>
+      </template>
+			<span>{{darkMode ? "밝은 테마" : "어두운 테마"}}</span>
+    </v-tooltip>
+
     <v-dialog v-if="member" v-model="dialog" persistent max-width="500">
       <v-card>
         <v-toolbar>
@@ -45,12 +50,19 @@
 <script>
 import { mapActions, mapState } from "vuex";
 import UserUpdateForm from "../Auth/UserUpdateForm.vue";
+import TooltipBtn from "../etc/TooltipBtn.vue";
 import DisplayAvatar from "./DisplayAvatar.vue";
 import MemberMenu from "./MemberMenu.vue";
 import NoMemberMenu from "./NoMemberMenu.vue";
 
 export default {
-  components: { DisplayAvatar, MemberMenu, NoMemberMenu, UserUpdateForm },
+  components: {
+    DisplayAvatar,
+    MemberMenu,
+    NoMemberMenu,
+    UserUpdateForm,
+    TooltipBtn,
+  },
   name: "SiteUser",
   data() {
     return {
@@ -138,9 +150,9 @@ export default {
         this.closeDialog();
         const mb_name = await this.signOut();
         this.$toast.info(`${mb_name}님 탈퇴 하였습니다.`);
-       	if(this.$route.name != 'Home') {
-					this.$router.push('/');
-				}
+        if (this.$route.name != "Home") {
+          this.$router.push("/");
+        }
       }
     },
     async checkEmail(email) {
