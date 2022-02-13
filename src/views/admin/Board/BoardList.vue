6b4744f2168aecc67c10d64ff59bd30460d14818
Kiyong Nam <moosin76@gmail.com>
2022-02-13 17:36:41 +0900

    065 게시판관리자 폼1

---
+++ src/views/admin/Board/BoardList.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-container fluid>
+		<v-toolbar>
+			<v-toolbar-title>게시판 관리</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<tooltip-btn label="게시판 추가" icon to="/adm/board/form">
+				<v-icon>mdi-plus</v-icon>
+			</tooltip-btn>
+		</v-toolbar>
+	</v-container>
+</template>
+
+<script>
+import TooltipBtn from '../../../components/etc/TooltipBtn.vue'
+export default {
+  components: { TooltipBtn },
+	name : "AdmBoardList"
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/admin/BoardRenderer.vue
@@ -0,0 +1,44 @@
+<template>
+  <board-list v-if="action=='list'"/>
+	<board-form v-else-if="action=='form'" :table="table"/>
+	<error v-else />
+	<!-- <component v-bind:is="viewComponent" :table="table"/> -->
+</template>
+
+<script>
+import BoardList from "./Board/BoardList.vue";
+import BoardForm from "./Board/BoardForm.vue";
+import Error from "../Error.vue";
+
+export default {
+  components: { BoardList, BoardForm, Error },
+  name: "AdmBoardRenderer",
+  computed: {
+    match() {
+      return this.$route.params.pathMatch.split("/");
+    },
+    action() {
+      return this.match[0];
+    },
+    table() {
+      return this.match[1] || "";
+    },
+    // viewComponent() {
+    //   switch (this.action) {
+    //     case "list":
+    //       return 'BoardList';
+    //     case "form":
+    //       return 'BoardForm';
+    //     default:
+    //       return 'Error';
+    //   }
+    // },
+  },
+  // mounted() {
+  //   console.log(this.$route);
+  // },
+};
+</script>
+
+<style>
+</style>
